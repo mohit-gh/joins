d56ef24ea22c4faea0b179c50947c66ec40f2b40
mohit-gh <mohitjajani85@gmail.com>
2018-05-02 12:56:18 +0530

    Update main.sql

--- main.sql
+++ main.sql
@@ -1,8 +1,17 @@
 Write a query fetch those customers who has not order yet using left join?
-
+___________________________________________________________________________________________________________________________________
 select Customers.customerid, customername
 from Customers left join orders
 on Customers.customerid = orders.customerid
 where orders.customerid is null
+
+----------------------------------------------------------------------------------------------------------------------------------
+
+Write a query to total order of customers?
+___________________________________________________________________________________________________________________________________
+select Customers.customerid, customername, count(orderid)
+from Customers left join orders
+on Customers.customerid = orders.customerid
+group by customername
 
 ----------------------------------------------------------------------------------------------------------------------------------
